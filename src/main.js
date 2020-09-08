a94f175a43d053c0b1a86b2fa7b9050dfd74a76e
丁正杰 <dingzhengjie@hoge.cn>
2020-09-08 15:38:31 +0800

    引入vue-cookies

--- src/main.js
+++ src/main.js
@@ -5,13 +5,25 @@ import App from './App'
 import router from './router'
 import * as api from './utils/api'
 import ElementUI from 'element-ui'
+import VueCookies from 'vue-cookies'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI)
+Vue.use(VueCookies)
 
 Vue.config.productionTip = false
 
 Vue.prototype.$api = api
+
+// router.beforeEach((to, from, next) => {
+//   console.log(to)
+//   if (to.fullPath === '/Login') {
+//     // login页面关闭header
+//     next({ path: to.path, query: { show_header: false, show_footer: false } })
+//   } else {
+//     next()
+//   }
+// })
 
 /* eslint-disable no-new */
 new Vue({
